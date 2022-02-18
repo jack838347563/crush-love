883d6d3dd1fdd248b2a9ba23aa9d074a995118a6
’jinyu‘ <jinyu@gmail.com>
2022-02-18 18:11:21 +0800

    first commit

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "做我的爱人我爱你",
+  descriptionHeader:
+      "如果您退出后不回复，则表示您同意^^",
+  buttonYes: "同意",
+  buttonNo: "拒绝! 不再想想嘛!",
+  titleModar: "感谢你的同意",
+  descriptionModar:
+    '我就知道你会同意做我的爱人^^ </br> 谢谢你同意让我保护和保护你。对我来说，你永远是个小女孩:)) </br> “和我一样喜欢你，你会明白的。</br>我喜欢你，你就会明白"',
+};
